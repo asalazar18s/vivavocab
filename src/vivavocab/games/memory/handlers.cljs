cc2edc605106fe9f4e000f9cef6cf846b4b2b340
Aaron Salazar <salazar.s.aaron@icloud.com>
2016-07-08 10:15:37 -0500

    Generate random list of cards

--- src/vivavocab/games/memory/handlers.cljs
+++ src/vivavocab/games/memory/handlers.cljs
@@ -1,45 +1,40 @@
 (ns vivavocab.games.memory.handlers
   (:require [re-frame.core :refer [register-handler]]))
 
+:words {1 {:id 1 :text "apple" :translation "manzana" :image "apple-image"}
+        5 {:id 5 :text "orange" :translation "naranja" :image "orange-image"}
+        7 {:id 7 :text "pear" :translation "pera" :image "pear-image"}
+        9 {:id 9 :text "banana" :translation "banana" :image "banana-image"}
+        10 {:id 10 :text "grapes" :translation "uvas" :image "grapes-image"}
+        12 {:id 12 :text "raspberry" :translation "frambuesa" :image "raspberry-image"}
+        15 {:id 15 :text "tangerine" :translation "mandarina" :image "tangerine-image"}
+        16 {:id 16 :text "pomegranate" :translation "granada" :image "pomegranate-image"}}
+
+(defn generate-list [state]
+      (let [word-ids (->> state
+                          :words
+                          keys
+                          shuffle
+                          (take 6)
+                          cycle
+                          (take 12)
+                          shuffle)
+            key-options (get-in state [:key-options])
+            cards (->> word-ids
+                       (map-indexed (fn [index word-id]
+                                        {:status :flipped
+                                         :index index
+                                         :word-id word-id
+                                         :word-key :text
+                                         :value word-id}))
+                       (reduce (fn [memo card]
+                                   (assoc memo (card :index) card)) {}))]
+           cards))
+
 (register-handler
   :memory/initialize
   (fn [state _]
-      (assoc-in state [:game :cards] {0 {:status :flipped
-                                         :index 0
-                                         :value 1}
-                                      1 {:status :flipped
-                                         :index 1
-                                         :value 2}
-                                      2 {:status :flipped
-                                         :index 2
-                                         :value 3}
-                                      3 {:status :flipped
-                                         :index 3
-                                         :value 4}
-                                      4 {:status :flipped
-                                         :index 4
-                                         :value 5}
-                                      5 {:status :flipped
-                                         :index 5
-                                         :value 6}
-                                      6 {:status :flipped
-                                         :index 6
-                                         :value 6}
-                                      7 {:status :flipped
-                                         :index 7
-                                         :value 5}
-                                      8 {:status :flipped
-                                         :index 8
-                                         :value 4}
-                                      9 {:status :flipped
-                                         :index 9
-                                         :value 3}
-                                      10 {:status :flipped
-                                          :index 10
-                                          :value 2}
-                                      11 {:status :flipped
-                                          :index 11
-                                          :value 1}})))
+      (assoc-in state [:game :cards] (generate-list state))))
 
 (register-handler
   :memory/flip-card
