02f0408d1a95a8bacb45cc031f33bd6b7a0fc371
Aaron Salazar <salazar.s.aaron@icloud.com>
2016-07-12 09:40:24 -0500

    Remove unnecessary code

--- src/vivavocab/games/memory/handlers.cljs
+++ src/vivavocab/games/memory/handlers.cljs
@@ -1,15 +1,6 @@
 (ns vivavocab.games.memory.handlers
   (:require [re-frame.core :refer [register-handler]]))
 
-:words {1 {:id 1 :text "apple" :translation "manzana" :image "apple-image"}
-        5 {:id 5 :text "orange" :translation "naranja" :image "orange-image"}
-        7 {:id 7 :text "pear" :translation "pera" :image "pear-image"}
-        9 {:id 9 :text "banana" :translation "banana" :image "banana-image"}
-        10 {:id 10 :text "grapes" :translation "uvas" :image "grapes-image"}
-        12 {:id 12 :text "raspberry" :translation "frambuesa" :image "raspberry-image"}
-        15 {:id 15 :text "tangerine" :translation "mandarina" :image "tangerine-image"}
-        16 {:id 16 :text "pomegranate" :translation "granada" :image "pomegranate-image"}}
-
 (defn generate-list [state]
       (let [cards (->> state
                        :words
