c7869ed466dfefa20de6ad2b9dbb644b1ce6f56c
Aaron Salazar <salazar.s.aaron@icloud.com>
2016-07-12 22:03:59 -0500

    Flip two cards and check if correct

--- src/vivavocab/games/memory/handlers.cljs
+++ src/vivavocab/games/memory/handlers.cljs
@@ -32,13 +32,55 @@
 (register-handler
   :memory/initialize
   (fn [state _]
-      (assoc-in state [:game :cards] (generate-list state))))
+      (assoc-in state [:game] {:cards (generate-list state)
+                               :flipped-count 0})))
 
 (defn flip-card-up [state card-index]
-      (assoc-in state [:game :cards card-index :status] :back))
+      (-> state
+          (assoc-in [:game :cards card-index :status] :back)
+          (update-in [:game :flipped-count] inc)))
+
+(defn flip-card-back [state card-index]
+      (-> state
+          (assoc-in [:game :cards card-index :status] :flipped)))
+
+(defn remove-card [state card-index]
+      (-> state
+          (assoc-in [:game :cards card-index :status] :gone)))
+
+(defn reset-flipped-count [state]
+      (assoc-in state [:game :flipped-count] 0))
+
+(defn flip-back-or-remove-cards [state]
+      (let [flipped-cards (->> (get-in state [:game :cards])
+                               vals
+                               (filter (fn [card]
+                                           (= (card :status) :back))))]
+           (tee flipped-cards)
+      (if (= (:word-id (first flipped-cards))
+             (:word-id (last flipped-cards)))
+        (-> state
+            (remove-card (:index (first flipped-cards)))
+            (remove-card (:index (last flipped-cards))))
+        (-> state
+            (flip-card-back (:index (first flipped-cards)))
+            (flip-card-back (:index (last flipped-cards)))))))
+
+(defn check-choices [state]
+      (let [flipped-count (get-in state [:game :flipped-count])]
+           (if (= flipped-count 2)
+             (-> state
+                 (reset-flipped-count)
+                 (flip-back-or-remove-cards))
+             state)))
 
 (register-handler
   :memory/flip-card
   (fn [state [_ card]]
       (-> state
-          (flip-card-up (card :index)))))
+          (flip-card-up (card :index))
+          (check-choices)
+          tee
+
+          ; when 2 flipped: reset flipped count, remove cards if match, etc.
+          )))
