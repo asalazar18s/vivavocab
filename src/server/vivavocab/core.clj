423665bcb5a4e7089d015310b0d6fa847814416e
Aaron Salazar <salazar.s.aaron@icloud.com>
2016-09-29 12:48:58 -0500

    Serve index and resource files

--- src/server/vivavocab/core.clj
+++ src/server/vivavocab/core.clj
@@ -2,13 +2,20 @@
     (:require
       [compojure.core :refer :all]
       [compojure.route :as route]
-      [org.httpkit.server :refer [run-server]]))
+      [org.httpkit.server :refer [run-server]]
+      [clojure.java.io :as io]))
 
 (defroutes app
-           (GET "/" [] "<h1>Hello World</h1>")
+           (GET "/" []
+                (-> "public/index.html"
+                    io/resource
+                    io/file
+                    slurp))
+           (GET "/api/data" [] ) ;TODO return game data
+           (route/resources "/")
            (route/not-found "<h1>Page not found</h1>"))
 
-(def server (atom nil))
+(defonce server (atom nil))
 
 (defn start! [port]
       (println "Starting web server on port" port)
